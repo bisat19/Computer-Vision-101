c7a5090903cb082808cbacd939f1198c220305c3
Abi Satya Deville <118191553+bisat19@users.noreply.github.com>
2024-03-21 10:57:00 +0700

    Update fract.py

--- fract.py
+++ fract.py
@@ -1,14 +1,11 @@
 import turtle
-
-# Create a turtle screen and set its background color
 screen = turtle.Screen()
 screen.bgcolor("white")
 
-# Create a new turtle object
 my_turtle = turtle.Turtle()
 my_turtle.color("black")
 
-# Draw a square fractal
+
 def draw_square_fractal(turtle, length, depth):
     if depth == 0:
         return
@@ -31,13 +28,9 @@ def draw_square_fractal(turtle, length, depth):
     turtle.right(90)
     draw_square_fractal(turtle, length/3, depth-1)
 
-# Set the length of the square and the depth of the recursion
 length = 300
 depth = 3
-
-# Draw the fractal
 draw_square_fractal(my_turtle, length, depth)
 
-# Hide the turtle and close the turtle graphics window
 my_turtle.hideturtle()
 turtle.done()
